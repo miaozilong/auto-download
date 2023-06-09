e6c5c5020f57edeebf1be2c93540bdadcf5c04bb
miaozilong <miao.zilong@outlook.com>
2023-06-09 14:01:05 +0800

    设置为浅克隆

--- main.go
+++ main.go
@@ -23,39 +23,39 @@ func init() {
 	_ = log.ReplaceLogger(logger)
 	log.Info("项目启动")
 }
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
+func download(w http.ResponseWriter, r *http.Request) {
 	var body Body
 	var downloadByte []byte
 	json.NewDecoder(r.Body).Decode(&body)
 	url := body.Url
+	log.Debug("接收到下载请求", url)
 	projectName := getProjectNameFromUrl(url)
 	nowStr := time.Now().Format("20060102150405")
 	cloneTimePath := "/download/" + nowStr
 	clonePath := cloneTimePath + "/" + projectName
-	cmd := exec.Command("git", "clone", url, clonePath)
+	cmd := exec.Command("git", "clone", "--depth", "1", url, clonePath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Info(err)
 	}
 	s := string(output)
-	log.Info("result =>", s)
+	log.Info("下载成功，内容为:\n" + s)
 	zipFilePath := cloneTimePath + "/" + projectName + ".zip"
 	_ = zip.Zip(zipFilePath, clonePath)
+	log.Info("压缩成功")
 	//设置响应头
 	header := w.Header()
 	header.Add("Content-Type", "application/octet-stream")
-	fileSuffix := ".tar.gz"
 	header.Add("Content-Disposition", "attachment;filename="+projectName+".zip")
 	if downloadByte, err = os.ReadFile(zipFilePath); err != nil {
-		log.Info("找不到指定设备的升级文件tar.gz，使用默认文件")
-		downloadByte, _ = os.ReadFile("./update_package/" + "device00000" + fileSuffix)
+		log.Info("读取文件失败")
 	}
 	//写入到响应流中
 	w.Write(downloadByte)
 }
 func main() {
-	http.HandleFunc("/download", sayhelloName) //设置访问的路由
-	err := http.ListenAndServe(":8081", nil)   //设置监听的端口
+	http.HandleFunc("/download", download)   //设置访问的路由
+	err := http.ListenAndServe(":8081", nil) //设置监听的端口
 	if err != nil {
 		log.Info("ListenAndServe: ", err)
 	}
